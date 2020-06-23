8dfd14ab35e96964f9ed27d82e74cf280484dbef
AlexY99 <ajuarezf401@alumno.ipn.mx>
2020-06-22 22:53:20 -0500

    Final C4

--- src/main/java/utilerias/Utilerias.java
+++ src/main/java/utilerias/Utilerias.java
@@ -11,49 +11,87 @@ import javax.mail.internet.MimeMessage;
  *
  * @author azul-
  */
-public class Utilerias {
 
-    public void enviarEmail(String correoDst, String asunto, String texto) {
-        try {
+public class Utilerias {
+    
+    public void enviarEmail(String correoDst, String asunto, String texto){
+        try{
             System.out.println(correoDst);
             System.out.println(asunto);
             System.out.println(texto);
-            Properties p = new Properties();
-            p.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+            Properties p =new Properties();
             p.setProperty("mail.smtp.host", "smtp.gmail.com");
             p.setProperty("mail.smtp.starttls.enable", "true");
             p.setProperty("mail.smtp.starttls.required", "true");
-            p.put("mail.smtp.socketFactory.port", "465");
-            p.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            p.put("mail.smtp.auth", "true");
-            p.put("mail.smtp.port", "465");
-            p.setProperty("mail.smtp.user",
-                    "escuelawebmordekaiser@gmail.com");
+            p.setProperty("mail.smtp.port", "587");
+            p.setProperty("mail.smtp.user", 
+                    "webApDevEscom@gmail.com");
             p.setProperty("mail.smtp.auth", "true");
-
-//            Session session = Session.getInstance(p,
-//                    new javax.mail.Authenticator() {
-//                        protected PasswordAuthentication getPasswordAuthentication(){
-//                            return new PasswordAuthentication("webApDevEscom@gmail.com", "123webA69");
-//                        }
-//                    });
-//      
+      
             Session s = Session.getDefaultInstance(p);
-            MimeMessage mensaje = new MimeMessage(s);
+            MimeMessage mensaje= new MimeMessage(s);
             mensaje.setFrom(
-                    new InternetAddress("escuelawebmordekaiser@gmail.com"));
-            mensaje.addRecipients(Message.RecipientType.TO, correoDst);
-
+                    new InternetAddress("webApDevEscom@gmail.com"));
+            mensaje.addRecipients(Message.RecipientType.TO,correoDst);
+            
             mensaje.setSubject(asunto);
             mensaje.setText(texto);
-
+            
             Transport t = s.getTransport("smtp");
-            t.connect("escuelawebmordekaiser@gmail.com",
-                    "KilexUzi19");
+            t.connect("webApDevEscom@gmail.com",
+                    "123webA69");
             t.sendMessage(mensaje, mensaje.getAllRecipients());
             t.close();
-        } catch (Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
 }
+
+
+//public class Utilerias {
+//
+//    public void enviarEmail(String correoDst, String asunto, String texto) {
+//        try {
+//            System.out.println(correoDst);
+//            System.out.println(asunto);
+//            System.out.println(texto);
+//            Properties p = new Properties();
+//            p.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+//            p.setProperty("mail.smtp.host", "smtp.gmail.com");
+//            p.setProperty("mail.smtp.starttls.enable", "true");
+//            p.setProperty("mail.smtp.starttls.required", "true");
+//            p.put("mail.smtp.socketFactory.port", "465");
+//            p.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//            p.put("mail.smtp.auth", "true");
+//            p.put("mail.smtp.port", "465");
+//            p.setProperty("mail.smtp.user",
+//                    "escuelawebmordekaiser@gmail.com");
+//            p.setProperty("mail.smtp.auth", "true");
+//
+////            Session session = Session.getInstance(p,
+////                    new javax.mail.Authenticator() {
+////                        protected PasswordAuthentication getPasswordAuthentication(){
+////                            return new PasswordAuthentication("webApDevEscom@gmail.com", "123webA69");
+////                        }
+////                    });
+////      
+//            Session s = Session.getDefaultInstance(p);
+//            MimeMessage mensaje = new MimeMessage(s);
+//            mensaje.setFrom(
+//                    new InternetAddress("escuelawebmordekaiser@gmail.com"));
+//            mensaje.addRecipients(Message.RecipientType.TO, correoDst);
+//
+//            mensaje.setSubject(asunto);
+//            mensaje.setText(texto);
+//
+//            Transport t = s.getTransport("smtp");
+//            t.connect("escuelawebmordekaiser@gmail.com",
+//                    "KilexUzi19");
+//            t.sendMessage(mensaje, mensaje.getAllRecipients());
+//            t.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
